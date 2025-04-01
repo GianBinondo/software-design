d419461af65209a03468ef19ac2bab9128a87c7b
gianb <gianb@DESKTOP-BTVQ7CJ>
2025-04-01 22:21:35 +0800

    Added Week3

--- lab3/Form1.Designer.cs
+++ lab3/Form1.Designer.cs
@@ -20,20 +20,53 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnShowInfo = new Button();
+            listBoxBooks = new ListBox();
+            SuspendLayout();
+            // 
+            // btnShowInfo
+            // 
+            btnShowInfo.Location = new Point(331, 309);
+            btnShowInfo.Name = "btnShowInfo";
+            btnShowInfo.Size = new Size(143, 64);
+            btnShowInfo.TabIndex = 0;
+            btnShowInfo.Text = "Show Information";
+            btnShowInfo.UseVisualStyleBackColor = true;
+            btnShowInfo.Click += btnShowInfo_Click;
+            // 
+            // listBoxBooks
+            // 
+            listBoxBooks.FormattingEnabled = true;
+            listBoxBooks.ItemHeight = 15;
+            listBoxBooks.Location = new Point(132, 66);
+            listBoxBooks.Name = "listBoxBooks";
+            listBoxBooks.Size = new Size(561, 214);
+            listBoxBooks.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AccessibleRole = AccessibleRole.IpAddress;
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(listBoxBooks);
+            Controls.Add(btnShowInfo);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private Button btnShowInfo;
+        private ListBox listBoxBooks;
+    }
 }
--- lab3/Form1.cs
+++ lab3/Form1.cs
@@ -1,10 +1,96 @@
+using System.Net;
+using static lab3.Form1;
+
 namespace lab3
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Book
+        {
+            public string? Title { get; set; }
+            public string? Author { get; set; }
+
+            public virtual string GetInfo()
+            {
+                return $"{Title} by {Author}";
+            }
+        }
+
+        public class Magazine : Book
+        {
+            public int IssueNumber { get; set; }
+
+            public override string GetInfo()
+            {
+                return $"{Title}-Issue{IssueNumber}";
+            }
+        }
+
+        public class EBook : Book
+        {
+            public string? Format { get; set; }
+            public double FileSizeMB { get; set; }
+
+            public override string GetInfo()
+            {
+                return $"{Title} by {Author} [{Format}, {FileSizeMB}MB] (EBook)";
+            }
+        }
+
+        public class TextBook : Book
+        {
+            public int GradeLevel { get; set; }
+            public string? Subject { get; set; }
+
+            public override string GetInfo()
+            {
+                return $"{Title} by {Author} [{Subject} textbook for grade {GradeLevel}]";
+            }
+
+        }
+
+        public class AudioBook : Book
+        {
+            public string? Narrator { get; set; }
+            public TimeSpan Duration { get; set; }
+
+            public override string GetInfo()
+            {
+                return $"{Title} by {Author} [Narrated by {Narrator}, {Duration.TotalHours:0.0} hours]";
+            }
+        }
+
+        private void ProcessBooks(Book[] books)
+        {
+            listBoxBooks.Items.Clear();
+            foreach (var book in books)
+            {
+                listBoxBooks.Items.Add(book.GetInfo());
+            }
+        }
+
+        private void btnShowInfo_Click(object sender, EventArgs e)
+        {
+            Book[] library = new Book[]
+            {
+                new Book { Title = "Deep Rock Galactic", Author = "Karl" },
+                new Magazine { Title = "Tech Weekly", Author = "John Doe", IssueNumber = 45 },
+                new EBook { Title = "Invincible", Author = "Robert kirkman", Format = "PDF", FileSizeMB = 3000 },
+                new Magazine { Title = "Yeah Science! ", Author = "Walter White", IssueNumber = 102 },
+                new EBook { Title = "Flint and Steel", Author = "Steve", Format = "EPUB", FileSizeMB = 8.2 },
+                new Magazine {Title = "The art of war", Author ="Sun Tzu", IssueNumber = 13},
+                new TextBook { Title = "Advanced Mathematics", Author = "Dr. Sheldon Cooper", Subject = "Math", GradeLevel = 12 },
+                new AudioBook { Title = "Dying Light", Author = "Kyle Crane", Narrator = "Rick Sanchez", Duration = TimeSpan.FromHours(50) },
+                new TextBook { Title = "Rizz Fundamentals", Author = "Dr. Dolph Rizzler", Subject = "Biology", GradeLevel = 11 },
+                new AudioBook { Title = "The Hood", Author = "Barack Obama", Narrator = "Barack Obama", Duration = TimeSpan.FromHours(19) },
+                new Book { Title = " Toffe The Dog", Author = " Brownie"}
+            };
+            ProcessBooks(library); // Process them polymorphically
+        }
+    }
 }
